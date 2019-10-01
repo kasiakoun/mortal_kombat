f180590462b4270c725f01615971026221a2653d
kasaikoun <kasiakou.n@gmail.com>
2019-10-02 00:14:13 +0300

    Add MoveControlller class that moves Unit in the space with a defined frequency Add moveForward method that moves unit forward with a defined frequency

---
+++ src/core/game_space/move_controller.js
@@ -0,0 +1,54 @@
+import Point from '../point';
+
+/**
+ * @typedef {import('../units/unit_base').default} UnitBase
+ */
+
+export default class MoveController {
+  /**
+   * @param {UnitBase} unitBase
+   */
+  constructor(unitBase) {
+    /**
+     * @private
+     */
+    this.internal = {
+      /**
+       * @type {UnitBase}
+       */
+      unit: unitBase,
+      /**
+       * @type {NodeJS.Timeout}
+       */
+      timer: undefined,
+      /**
+       * @type {number}
+       */
+      frequency: 30,
+      /**
+       * @type {number}
+       */
+      forwardStep: 3,
+    };
+  }
+
+  moveForward() {
+    if (this.internal.timer) this.stop();
+
+    this.internal.timer = setInterval(() => {
+      const unitPosition = this.internal.unit.position;
+
+      const currentX = unitPosition.x + this.internal.forwardStep;
+      const currentY = unitPosition.y;
+
+      this.internal.unit.position = new Point(currentX, currentY);
+    }, this.internal.frequency);
+  }
+
+  stop() {
+    if (!this.internal.timer) return;
+
+    clearInterval(this.internal.timer);
+    this.internal.timer = undefined;
+  }
+}

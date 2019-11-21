8bb6ad1310136a3eedc7edfc1a9a633797e1ec8f
kasaikoun <kasiakou.n@gmail.com>
2019-11-22 00:14:52 +0300

    Add a function helper to load a file by path Add a function helper to load JSON object by path

---
+++ src/core/file_helpers/load_json_object.js
@@ -0,0 +1,14 @@
+import loadTextFile from './load_text_file';
+
+/**
+ * Loads a JSON object by path
+ * @param {string} jsonPath A path to the JSON file
+ * @returns {any} Returns a JSON object by path if a request is failed then returns undefined
+ */
+function loadJsonObject(jsonPath) {
+  const mimeType = 'application/json';
+  const jsonText = loadTextFile(jsonPath, mimeType);
+  return JSON.parse(jsonText);
+}
+
+export default loadJsonObject;

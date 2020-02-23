962a8c99aa7d6472eed103c25b8000aa952da902
kasaikoun <kasiakou.n@gmail.com>
2020-02-24 00:39:56 +0300

    Add handleInput method to UnitBase class which handles a player input and sets currentState of Unit on a basis of input

--- src/core/entities/units/unit_base.js
+++ src/core/entities/units/unit_base.js
@@ -1,11 +1,16 @@
 import MoveController from '../../game_space/move_controller';
 import Entity from '../entity';
+import StanceState from './states/stance_state';
 
 /**
  * A base class for units
  * @typedef {import('../../transform').default} Transform
  * @typedef {import('../../animation/sprite_sheet').default} SpriteSheet
  * @typedef {import('../../game_space/move_enabler').default} MoveEnabler
+ * @typedef {import('./states/state_base').default} StateBase
+ * @typedef {import('../../player/input_event_type').default} InputEventType
+ * @typedef {import('../../player/input_type').default} InputType
+ * @typedef {import('../../player/input_state').default} InputState
  */
 class UnitBase extends Entity {
   get transform() {
@@ -23,6 +28,10 @@ class UnitBase extends Entity {
     return this.internal.moveController;
   }
 
+  get currentState() {
+    return this.internal.currentState;
+  }
+
   /**
    * @param {Transform} transform
    * @param {SpriteSheet} spriteSheet
@@ -35,12 +44,27 @@ class UnitBase extends Entity {
      * @type {{
      * transform: Transform,
      * spriteSheet: SpriteSheet,
-     * moveController: MoveController}}
+     * moveController: MoveController,
+     * currentState: StateBase}}
      */
     this.internal = Object.assign({}, this.internal);
 
     this.internal.spriteSheet = spriteSheet;
     this.internal.moveController = new MoveController(this, moveEnabler);
+    this.internal.currentState = new StanceState(this);
+  }
+
+  /**
+   * @param {InputEventType} inputEventType
+   * @param {InputType} inputType
+   * @param {InputState} inputState
+   */
+  handleInput(inputEventType, inputType, inputState) {
+    const newState = this.internal.currentState.handleInput(inputEventType, inputType, inputState);
+
+    if (newState) {
+      this.internal.currentState = newState;
+    }
   }
 }
 

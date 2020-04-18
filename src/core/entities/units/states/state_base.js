d6134b74b71507f0eba413cd2b015f1b78efaec6
kasaikoun <kasiakou.n@gmail.com>
2020-04-18 14:44:21 +0300

    Add lastInputState field and stateCompleted event to StateBase class Modify all states classes so that handleInput method calls handleInput method of StateBase class Add JumpUpwardState class which contais logic for jumping of Unit

--- src/core/entities/units/states/state_base.js
+++ src/core/entities/units/states/state_base.js
@@ -1,15 +1,33 @@
+import Observable from '../../../observable';
+
 /**
  * @typedef {import('../unit_base').default} UnitBase
- * @typedef {import('../../player/input_event_type').default} InputEventType
- * @typedef {import('../../player/input_type').default} InputType
- * @typedef {import('../../player/input_state').default} InputState
+ * @typedef {import('../../../player/input_event_type').default} InputEventType
+ * @typedef {import('../../../player/input_type').default} InputType
+ * @typedef {import('../../../player/input_state').default} InputState
  */
 class StateBase {
+  get stateCompleted() {
+    return this.internal.stateCompleted;
+  }
+
   /**
    * @param {UnitBase} unit
+   * @param {InputState} [lastInputState]
    */
-  constructor(unit) {
+  constructor(unit, lastInputState) {
+    /**
+     * @type {{
+     * lastInputState: InputState,
+     * stateCompleted: Observable
+     * }}
+     */
+    this.internal = {};
+
     this.unit = unit;
+    this.internal.lastInputState = lastInputState;
+
+    this.internal.stateCompleted = new Observable();
     this.promote();
   }
 
@@ -25,7 +43,7 @@ class StateBase {
    */
   // eslint-disable-next-line no-unused-vars
   handleInput(inputEventType, inputType, inputState) {
-    throw new Error(`handleState is not implemented in '${this.constructor.name}' class`);
+    this.internal.lastInputState = inputState;
   }
 }
 
--- src/core/entities/units/states/walk_forward_state.js
+++ src/core/entities/units/states/walk_forward_state.js
@@ -4,6 +4,7 @@ import StanceState from './stance_state';
 import InputEventType from '../../../player/input_event_type';
 import InputType from '../../../player/input_type';
 import WalkBackwardState from './walk_backward_state';
+import JumpUpwardState from './jump_upward_state';
 
 /**
  * @typedef {import('../unit_base').default} UnitBase
@@ -22,6 +23,7 @@ class WalkForwardState extends StateBase {
    * @returns {StateBase}
    */
   handleInput(inputEventType, inputType, inputState) {
+    super.handleInput(inputEventType, inputType, inputState);
     let newState;
 
     switch (inputType) {
@@ -29,6 +31,8 @@ class WalkForwardState extends StateBase {
         if (inputEventType === InputEventType.up) {
           if (inputState.backward) {
             newState = new WalkBackwardState(this.unit);
+          } else if (inputState.upward) {
+            newState = new JumpUpwardState(this.unit);
           } else {
             newState = new StanceState(this.unit);
           }
